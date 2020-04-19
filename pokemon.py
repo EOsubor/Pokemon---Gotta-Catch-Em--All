023ecf242d27bd217f409492dcfb07726e593f96
EOsubor <ekeneosubor@gmail.com>
2020-04-19 13:13:12 +0100

    Refactoring/Extra mehods
    Added pikachu as a starter pokemon
    Tweaked damage and health stats.

--- pokemon.py
+++ pokemon.py
@@ -9,10 +9,12 @@ class Pokemon:
     self.current_health = current_health
     self.is_ko = is_ko
 
+
   def __repr__(self):
     return "Pokemon info: {}, current level: {}, type: {}, maximun health: {},
      current health: {}.\n".format(self.name, self.level, self.poke_type, self.max_health,
       self.current_health)
+
 
   def lose_health(self, damage):
     health_rem = self.current_health - damage
@@ -23,10 +25,12 @@ class Pokemon:
     else:
       self.knock_out()
 
+
   def knock_out(self):
     if self.current_health <= 0:
       print(f"{self.name.title()} has been knocked out.")
       self.is_ko = True
+
 
   def revive_pokemon(self):
     self.current_health = round(self.max_health / 2)
@@ -41,6 +45,7 @@ class Pokemon:
     # time.sleep(1) between print statements to simulate healing over time. Potion?
     pass
 
+
   def attack(self, other):
       bonus_dmg = 5
       for p_type in self.poke_type:
@@ -48,45 +53,45 @@ class Pokemon:
             if self.level < 10:
                 # Refactor dmg/poke_type code below into function. DRY
     			if (p_type == 'fire' and o_p_type == 'grass') \
-    				or (t == 'grass' and o_p_type == 'water') \
-    				or (t == 'water' and o_p_type == 'fire'):
+    				or (p_type == 'grass' and o_p_type == 'water') \
+    				or (p_type == 'water' and o_p_type == 'fire')
+                    or (p_type == 'electric' and o_p_type == 'water'):
     				damage = (random.randint(1, 6) * 5) + bonus_dmg
     		    elif (p_type == 'grass' and o_p_type == 'fire') \
     			    or (p_type == 'fire' and o_p_type == 'water') \
-    			    or (p_type == 'water' and o_p_type == 'grass'):
+    			    or (p_type == 'water' and o_p_type == 'grass')
+                    or (p_type == 'water' and o_p_type == 'electric'):
                     damage = (random.randint(1, 6) * 5) - bonus_dmg
 
             elif (self.level > 9) and (self.level < 20):
-    			if (t == 'fire' and y == 'grass') \
-    				or (t == 'grass' and y == 'water') \
-    				or (t == 'water' and y == 'fire'):
+    			if (p_type == 'fire' and o_p_type == 'grass') \
+    				or (p_type == 'grass' and o_p_type == 'water') \
+    				or (p_type == 'water' and o_p_type == 'fire'):
     				damage = (random.randint(6, 11) * 5) + bonus_dmg
-    		    elif (t == 'grass' and y == 'fire') \
-    			    or (t == 'fire' and y == 'water') \
-    			    or (t == 'water' and y == 'grass'):
+    		    elif (p_type == 'grass' and o_p_type == 'fire') \
+    			    or (p_type == 'fire' and o_p_type == 'water') \
+    			    or (p_type == 'water' and o_p_type == 'grass'):
                     damage = (random.randint(6, 11) * 5) - bonus_dmg
 
             elif (self.level > 20) and (self.level < 30):
-                if (t == 'fire' and y == 'grass') \
-    				or (t == 'grass' and y == 'water') \
-    				or (t == 'water' and y == 'fire'):
+                if (p_type == 'fire' and o_p_type == 'grass') \
+    				or (p_type == 'grass' and o_p_type == 'water') \
+    				or (p_type == 'water' and o_p_type == 'fire'):
     				damage = (random.randint(11, 21) * 5) + bonus_dmg
-    		    elif (t == 'grass' and y == 'fire') \
-    			    or (t == 'fire' and y == 'water') \
-    			    or (t == 'water' and y == 'grass'):
+    		    elif (p_type == 'grass' and o_p_type == 'fire') \
+    			    or (p_type == 'fire' and o_p_type == 'water') \
+    			    or (p_type == 'water' and o_p_type == 'grass'):
                     damage = (random.randint(11, 21) * 5) - bonus_dmg
             else:
-                if (t == 'fire' and y == 'grass') \
-    				or (t == 'grass' and y == 'water') \
-    				or (t == 'water' and y == 'fire'):
+                if (p_type == 'fire' and o_p_type == 'grass') \
+    				or (p_type == 'grass' and o_p_type == 'water') \
+    				or (p_type == 'water' and o_p_type == 'fire'):
     				damage = (random.randint(30, 50) * 5) + bonus_dmg
-    		    elif (t == 'grass' and y == 'fire') \
-    			    or (t == 'fire' and y == 'water') \
-    			    or (t == 'water' and y == 'grass'):
+    		    elif (p_type == 'grass' and o_p_type == 'fire') \
+    			    or (p_type == 'fire' and o_p_type == 'water') \
+    			    or (p_type == 'water' and o_p_type == 'grass'):
                     damage = (random.randint(30, 50) * 5) - bonus_dmg
 
-    if self.type.lower() == 'fire':
-      pass
 
 
 class Legendary(Pokemon):
